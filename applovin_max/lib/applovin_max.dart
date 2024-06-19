eb64c0a43fece38c0c1ebcf2d8cb06f7f093607e
Hiroshi Watanabe <hiroshi.watanabe@applovin.com>
2024-06-19 19:41:59 +0900

    3.10.1

--- applovin_max/lib/applovin_max.dart
+++ applovin_max/lib/applovin_max.dart
@@ -16,7 +16,7 @@ export 'package:applovin_max/src/targeting_data.dart';
 /// Represents the AppLovin SDK.
 class AppLovinMAX {
   /// The current version of the SDK.
-  static const version = "3.10.0";
+  static const version = "3.10.1";
 
   /// @nodoc
   static MethodChannel channel = const MethodChannel('applovin_max');
