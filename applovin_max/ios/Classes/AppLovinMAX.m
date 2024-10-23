acaf19bfd5a1523e69a690d0f023c651ba85ac9c
Hiroshi Watanabe <hiroshi.watanabe@applovin.com>
2024-10-23 16:44:52 +0900

    4.0.2

--- applovin_max/ios/Classes/AppLovinMAX.m
+++ applovin_max/ios/Classes/AppLovinMAX.m
@@ -53,7 +53,7 @@
 @implementation AppLovinMAX
 static NSString *const SDK_TAG = @"AppLovinSdk";
 static NSString *const TAG = @"AppLovinMAX";
-static NSString *const PLUGIN_VERSION = @"4.0.0";
+static NSString *const PLUGIN_VERSION = @"4.0.2";
 
 static NSString *const USER_GEOGRAPHY_GDPR = @"G";
 static NSString *const USER_GEOGRAPHY_OTHER = @"O";
@@ -74,6 +74,7 @@ static NSDictionary<NSString *, NSString *> *ALCompatibleNativeSDKVersions;
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar
 {
     ALCompatibleNativeSDKVersions = @{
+        @"4.0.2" : @"13.0.0",
         @"4.0.1" : @"13.0.0",
         @"4.0.0" : @"13.0.0"
     };
